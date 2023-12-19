dee247a23b867d64e14a36e8376ef69b5e10ab0b
imran khan <imranloveh01@gmail.com>
2023-12-19 20:54:25 +0500

    Add shopping bag icon to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import { NAVIGATION } from "@/constants";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
+import { ShoppingBag } from "lucide-react";
 
 const Navbar = () => {
   const pathname = usePathname();
@@ -11,12 +12,12 @@ const Navbar = () => {
     <header className="mb-8 border-2">
       <div className="flex justify-between max-w-2xl mx-auto px-4 lg:max-w-7xl sm:px-6 items-center py-3">
         <Link href="/">
-          <h1 className="text-4xl font-bold">
+          <h1 className="md:text-4xl text-2xl font-bold">
             <span className="text-primary">E-</span>COMMERCE
           </h1>
         </Link>
 
-        <nav className="hidden gap-12 lg:flex 2xl:ml-16">
+        <nav className="hidden gap-12 lg:flex 2xl:ml-16 items-center">
           {NAVIGATION.map((nav, i) => {
             // const isActive =  styles based on pathname
             const isActive =
@@ -28,7 +29,7 @@ const Navbar = () => {
                 href={nav.path}
                 key={i}
                 className={`text-lg font-semibold ${
-                  isActive ? "text-white bg-primary" : "text-gray-700"
+                  isActive ? "text-white bg-primary px-3 rounded-sm py-1" : "text-gray-700"
                 }  hover:text-gray-900`}
               >
                 {nav.name}
@@ -37,8 +38,12 @@ const Navbar = () => {
           })}
         </nav>
         <div className="flex divide-x border-r sm:border-l">
-          <Button className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 md:h-24 md:w-24 rounded-none">
-            Cart
+          <Button
+            variant={"outline"}
+            className="flex flex-col gap-y-1.5 h-12 w-12 sm:h-20 md:h-24 md:w-24 rounded-none"
+          >
+            <ShoppingBag />
+            <span className="hidden sm:block text-xs font-semibold text-gray-500">Cart</span>
           </Button>
         </div>
       </div>
