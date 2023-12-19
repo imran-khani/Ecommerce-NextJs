542897fe224b28fdac3fa092e6004eec216b892b
imran khan <imranloveh01@gmail.com>
2023-12-19 18:41:38 +0500

    Add link to homepage in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <header className="mb-8 border-2">
       <div className="flex justify-between max-w-2xl mx-auto px-4 lg:max-w-7xl sm:px-6 items-center">
-        
+        <Link href="/" >
+            <h1 className="text-4xl font-bold">E-COMMERCE</h1>
+        </Link>
       </div>
     </header>
   );
