64e13690c8b8c34b26bd26d845b8095e017c4610
imran khan <imranloveh01@gmail.com>
2023-12-19 22:04:53 +0500

    Delete unused files and dependencies

--- sanity/schema.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import product from './schemas/product'
-import category from './schemas/category'
-import heroImages from './schemas/heroImages'
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product,category,heroImages],
-}
