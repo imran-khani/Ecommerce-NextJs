0ef763a23235364a270d52f69c1690aa20c6cace
imran khan <imranloveh01@gmail.com>
2023-12-20 01:56:35 +0500

    Refactor code for improved performance

---
+++ sanity-project/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ni2oj0zl',
+    dataset: 'production'
+  }
+})
---
+++ sanity-project/schemas/heroImages.ts
@@ -0,0 +1,17 @@
+export default {
+    name: 'heroImages',
+    title: 'Hero Images',
+    type: 'document',
+    fields: [
+        {
+            name: 'image1',
+            title: 'Image 1',
+            type:'image'
+        },
+         {
+            name: 'image2',
+            title: 'Image 2',
+            type:'image'
+         }
+    ]
+}
