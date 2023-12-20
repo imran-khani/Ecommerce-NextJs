3a3e165d598973739b1c9534614e736559e43dd9
imran khan <imranloveh01@gmail.com>
2023-12-20 20:14:34 +0500

    Add sanity client configuration

---
+++ app/lib/sanity.ts
@@ -0,0 +1,11 @@
+import { createClient } from 'next-sanity'
+
+export const client = createClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    useCdn: true,
+    apiVersion: '2022-03-25',
+})
+
+
+const builder = image
