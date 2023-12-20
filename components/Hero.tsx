acc558a1fbb7317e054f57b803ac15cc7645f841
imran khan <imranloveh01@gmail.com>
2023-12-20 21:22:13 +0500

    Add Link component to Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,14 +1,15 @@
 import { client, urlFor } from "@/app/lib/sanity";
 import Image from "next/image";
+import Link from "next/link";
 
 const fetchData = async () => {
-  const query = "*[_type=='heroImage'][0]"
-  const data = await client.fetch(query)
-  return data
-}
+  const query = "*[_type=='heroImage'][0]";
+  const data = await client.fetch(query);
+  return data;
+};
 
 const Hero = async () => {
-  const data = await fetchData()
+  const data = await fetchData();
   if (!data) {
     return <div>No data!</div>;
   }
@@ -26,17 +27,36 @@ const Hero = async () => {
         </div>
         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
-            <Image width={500} height={500} src={urlFor(data.heroImage1).url()} className="h-full w-full object-center object-cover" alt="heroImage1" />
+            <Image
+              width={500}
+              priority
+              height={500}
+              src={urlFor(data.heroImage1).url()}
+              className="h-full w-full object-center object-cover"
+              alt="heroImage1"
+            />
           </div>
           <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
             <Image
               src={urlFor(data.heroImage2).url()}
               width={500}
               height={500}
+              priority
               alt="heroImage2"
               className="h-full w-full object-center object-cover"
             />
           </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+        <div className="flex h-12 w-64 overflow-hidden divide-x rounded-lg border">
+          <Link
+            href={"/men"}
+            className="flex items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 w-1/3"
+          >
+            Men
+          </Link>
         </div>
       </div>
     </section>
