4f2b9e7a6b71ebcd03d02523706339a7bdbe4af2
imran khan <imranloveh01@gmail.com>
2023-12-20 23:29:36 +0500

    Refactor getData function and fix formatting in Newest component

--- components/Newest.tsx
+++ components/Newest.tsx
@@ -1,18 +1,23 @@
-import { client } from "@/app/lib/sanity"
+import { client } from "@/app/lib/sanity";
 
-     const getData = async ()=>{
-        const query = "*[_type == 'product']"
-        const data = await client.fetch(query)
-        return data
-     }
+const getData = async () => {
+  const query = `*[_type=='product'][0...4]{
+            _id,
+              name,
+              price,
+              "slug":slug.current,
+              "category":category->name,
+              "imageUrl":images[0].asset->url
+          }`;
+  const data = await client.fetch(query);
+  return data;
+};
 
-const Newest = async() => {
-    const products = await getData()
-    console.log('products: ', products);
-     
-  return (
-    <div>Newest</div>
-  )
-}
+const Newest = async () => {
+  const products = await getData();
+  console.log("products: ", products);
 
-export default Newest
+  return <div>Newest</div>;
+};
+
+export default Newest;
