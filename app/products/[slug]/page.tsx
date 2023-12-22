24a237b577a0e434c3aed76d3b17f2404217edd7
imran khan <imranloveh01@gmail.com>
2023-12-22 13:06:02 +0500

    Refactor product page layout and add checkout button

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -21,13 +21,24 @@ async function getData(slug: string) {
 
 const Product = async ({ params }: { params: { slug: string } }) => {
   const product: ProductPageTypes = await getData(params.slug);
-  console.log("product: ", product);
 
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-8 md:grid-cols-2">
           <ImageGallery images={product.images} />
+          <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold">{product.name}</h1>
+            <p className="text-gray-500">{product.description}</p>
+            <p className="text-gray-500">Price: {product.price}</p>
+            <button
+              className="bg-gray-900 text-white px-4 py-2 rounded-lg"
+              id="checkout-button"
+              role="link"
+            >
+              Checkout
+            </button>
+            </div>
         </div>
       </div>
     </div>
